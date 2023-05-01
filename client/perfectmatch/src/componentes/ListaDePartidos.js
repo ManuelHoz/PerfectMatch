c60f791c4df04c330c50c31e2905e1b6d671e4ae
Manuel_de_la_Hoz <manuel.dev@usm.cl>
2023-04-30 22:01:31 -0400

    se añade lista de usuarios y bootstrap

---
+++ client/perfectmatch/src/componentes/ListaDePartidos.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ListaDePartidos = () => {
+    return (
+    <div>
+        <h3>Lista de Partidos</h3>
+        <button className="btn btn-primary">Crear Partido</button>
+    </div>
+    );
+}; 
+
+export default ListaDePartidos;
