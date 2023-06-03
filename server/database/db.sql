a0608a44487a51c9f21d72be9d04da2423cd09ec
Manuel_de_la_Hoz <manuel.dev@usm.cl>
2023-06-03 14:18:19 -0400

    creacion de tablas sql

--- server/database/db.sql
+++ server/database/db.sql
@@ -1,7 +1,34 @@
 CREATE DATABASE taskdb;
 
-CREATE TABLE partido(
+CREATE TABLE evento(
     id SERIAL PRIMARY KEY,
     titulo VARCHAR(100) NOT NULL,
-    descripcion VARCHAR(255)
+    descripcion VARCHAR(255),
+    capacidad_de_personas INT NOT NULL,
 );
+
+CREATE TABLE PerfectMatcher(
+    id SERIAL PRIMARY KEY,
+    nombre VARCHAR(100) NOT NULL,
+    apellido VARCHAR(100) NOT NULL,
+    username VARCHAR(100) NOT NULL,
+    contraseña VARCHAR(100) NOT NULL,
+)
+
+CREATE TABLE evento_perfectmatcher(
+    id SERIAL PRIMARY KEY,
+    id_evento INT NOT NULL,
+    id_perfectmatcher INT NOT NULL,
+    FOREIGN KEY (id_evento) REFERENCES evento(id),
+    FOREIGN KEY (id_perfectmatcher) REFERENCES perfectmatcher(id)
+);
+
+CREATE TABLE admin(
+    id SERIAL PRIMARY KEY,
+    nombre VARCHAR(100) NOT NULL,
+    apellido VARCHAR(100) NOT NULL,
+    username VARCHAR(100) NOT NULL,
+    contraseña VARCHAR(100) NOT NULL,
+);
+
+
