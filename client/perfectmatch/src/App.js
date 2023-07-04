359d9ddb893df6738ca229e895c66d878ea0a221
Sebastian Araya <sebastian.arayapa@usm.cl>
2023-07-04 12:55:13 -0400

    Auth0 login

--- client/perfectmatch/src/App.js
+++ client/perfectmatch/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import { LoginButton } from './Login';
 import ListaDePartidos from './componentes/ListaDePartidos';
 import 'bootstrap/dist/css/bootstrap.min.css'
 
@@ -7,6 +8,7 @@ function App() {
   return (
     <div className="App">
       <header>
+        <LoginButton />
       </header>
       <body>
         <ListaDePartidos />
--- client/perfectmatch/src/index.js
+++ client/perfectmatch/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Auth0Provider } from '@auth0/auth0-react';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -7,7 +8,10 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Auth0Provider domain="dev-jv4lavmhugtmkyte.us.auth0.com" clientId='xQI7g5B6PSGqVWzvQcpgfF5TypQ83DUS' redirectUri={window.location.origin}>
+      <App />
+    </Auth0Provider>
+    
   </React.StrictMode>
 );
 
